143bf9533f2907c71bd9c52e20ec90d7d7a7db2e
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-08-20 01:22:01 +0200

    Mount HORIZON event graph router

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -36,3 +36,4 @@ from . import horizon_backfill as _horizon_backfill  # noqa: F401,E402
 from . import horizon_weather_chain as _horizon_weather_chain  # noqa: F401,E402
 from . import horizon_global_alerts as _horizon_global_alerts  # noqa: F401,E402
 from . import horizon_convergence as _horizon_convergence  # noqa: F401,E402
+from . import horizon_event_graph as _horizon_event_graph  # noqa: F401,E402
