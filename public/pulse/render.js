a2e05a630700d9ed7801a11a771257a5da2489b6
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-09-19 19:19:56 +0200

    Align Pulse module cache version V9

--- public/pulse/render.js
+++ public/pulse/render.js
@@ -1,4 +1,4 @@
-import { dom, esc, icon, initials, timeAgo, setStatus } from './core.js?v=8';
+import { dom, esc, icon, initials, timeAgo, setStatus } from './core.js?v=9';
 
 export function renderPost(post){
   const user=post.author||{};
