05936cc59dc00e0fc6514461d5f97036a886b7d7
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-09-19 19:11:27 +0200

    Align Pulse view module versions for rich media release

--- public/pulse/views.js
+++ public/pulse/views.js
@@ -1,6 +1,6 @@
-import { state, dom, api, esc, icon, initials, timeAgo, setStatus, errorText } from './core.js?v=7';
-import { requireAuth } from './session.js?v=7';
-import { renderPost, renderPosts, notificationLabel } from './render.js?v=7';
+import { state, dom, api, esc, icon, initials, timeAgo, setStatus, errorText } from './core.js?v=8';
+import { requireAuth } from './session.js?v=8';
+import { renderPost, renderPosts, notificationLabel } from './render.js?v=8';
 
 export function setView(view,title){
   state.view=view;
