b72e8d6a37d750fc4fe92d5e1218cbf5c38393e0
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-09-20 00:11:46 +0200

    Upgrade Identity Vault to encrypted personal profile v0.2.0

--- identity-vault/src/preload.cjs
+++ identity-vault/src/preload.cjs
@@ -2,7 +2,9 @@ const {contextBridge,ipcRenderer}=require("electron");
 contextBridge.exposeInMainWorld("IdentityVault",{
   status:()=>ipcRenderer.invoke("vault:status"),
   create:(options)=>ipcRenderer.invoke("vault:create",options),
+  loadFile:()=>ipcRenderer.invoke("vault:load-file"),
   unlock:(passphrase)=>ipcRenderer.invoke("vault:unlock",{passphrase}),
+  updateProfile:(profile)=>ipcRenderer.invoke("vault:update-profile",{profile}),
   lock:()=>ipcRenderer.invoke("vault:lock"),
   revealLocation:()=>ipcRenderer.invoke("vault:reveal-location")
 });
