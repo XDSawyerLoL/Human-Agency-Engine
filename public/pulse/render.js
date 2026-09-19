c4d102a4e3e6e801b3ec818c80bc1009d27fb8bd
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-09-19 19:04:40 +0200

    Version Pulse render dependencies

--- public/pulse/render.js
+++ public/pulse/render.js
@@ -1,4 +1,4 @@
-import { dom, esc, icon, initials, timeAgo, setStatus } from './core.js';
+import { dom, esc, icon, initials, timeAgo, setStatus } from './core.js?v=7';
 
 export function renderPost(post){
   const user=post.author||{};
