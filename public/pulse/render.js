844fd0d814391d52021042df05ef630b9b502612
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-09-21 11:20:40 +0200

    build: publish durable Pulse client v16

--- public/pulse/render.js
+++ public/pulse/render.js
@@ -1,4 +1,4 @@
-import { dom, esc, icon, initials, timeAgo, setStatus } from './core.js?v=15';
+import { dom, esc, icon, initials, timeAgo, setStatus } from './core.js?v=16';
 
 export function renderPost(post){
   const user=post.author||{};
