2e130f01d74cde4fbae68aad128f108ea5c328b0
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-09-21 10:50:12 +0200

    build: publish Pulse Secure decentralized client v14

--- public/pulse/session.js
+++ public/pulse/session.js
@@ -1,4 +1,4 @@
-import { TOKEN_KEY, state, dom, initials, api } from './core.js?v=13';
+import { TOKEN_KEY, state, dom, initials, api } from './core.js?v=14';
 
 const PRESENCE_RENEW_MS=5000;
 const PRESENCE_PROBE_MS=1000;
