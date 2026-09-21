60c7ad37dcb32c820016584a4962e41faeb1c6d1
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-09-21 11:16:18 +0200

    build: publish Pulse Secure prekey client v15

--- public/pulse/render.js
+++ public/pulse/render.js
@@ -1,4 +1,4 @@
-import { dom, esc, icon, initials, timeAgo, setStatus } from './core.js?v=14';
+import { dom, esc, icon, initials, timeAgo, setStatus } from './core.js?v=15';
 
 export function renderPost(post){
   const user=post.author||{};
