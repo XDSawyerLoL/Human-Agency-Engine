898d8c6964915e82425e88a2496f213840daddfd
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-09-21 09:59:07 +0200

    build: bust Pulse module cache after persistence fix

--- public/pulse/render.js
+++ public/pulse/render.js
@@ -1,4 +1,4 @@
-import { dom, esc, icon, initials, timeAgo, setStatus } from './core.js?v=9';
+import { dom, esc, icon, initials, timeAgo, setStatus } from './core.js?v=11';
 
 export function renderPost(post){
   const user=post.author||{};
