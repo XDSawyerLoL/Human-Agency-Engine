ec63e313b93778111711ae549b2c342d4541c714
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-09-21 12:13:40 +0200

    build: publish Pulse same-origin client v18

--- public/pulse/render.js
+++ public/pulse/render.js
@@ -1,4 +1,4 @@
-import { dom, esc, icon, initials, timeAgo, setStatus } from './core.js?v=17';
+import { dom, esc, icon, initials, timeAgo, setStatus } from './core.js?v=18';
 
 export function renderPost(post){
   const user=post.author||{};
